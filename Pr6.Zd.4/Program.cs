73c828641443781095e9353aa78f4a264adb4bda
USER <USER@DESKTOP-TRQG01D>
2022-10-07 17:01:29 +0300

    Добавьте файлы проекта.

---
+++ Pr6.Zd.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            Console.WriteLine("Введите 3 числа");
+            Console.Write("a= ");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.Write("b= ");
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.Write("c= ");
+            c = Convert.ToInt16(Console.ReadLine());
+            x = (a * b * c) / 2;
+            Console.WriteLine("Результат вычисления :{0}", x);
+
+        }
+    }
+}
